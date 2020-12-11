3526612a3a50728b9493644b507b14de81faf29d
Suresh Perikala <sperikala@gmail.com>
2020-12-11 12:51:39 +0530

    This is a change by me

--- cool_password_generator/settings.py
+++ cool_password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2g-l)rc1048&7ft9b1lfv(5ygggo^y1v$p+5%(n!@k5lq4!$7a'
+SECRET_KEY = '2g-l)rc1048&7fderfgthjkdcfvgbhnm,t9b1lfv(5ygggo^y1v$p+5%(n!@k5lq4!$7a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
